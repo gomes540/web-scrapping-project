76141a1c561d67317c9264fecb3a6dd0a13ff433
Felipe Gomes <felipegomes540@gmail.com>
2022-05-09 12:46:53 -0300

    feat: create dataframe of books informations

--- dags/cayena_etl/src/domain/web_scraping.py
+++ dags/cayena_etl/src/domain/web_scraping.py
@@ -59,3 +59,7 @@ def get_title_name_price_lists(all_books_information: List[str]) -> List:
 def count_books_copies(all_tiles: List[str]) -> dict:
   copies_num = dict(Counter(all_tiles))
   return copies_num
+
+def create_copies_dataframe(repeted_books: dict) -> pd.DataFrame:
+  copies_dataframe = pd.DataFrame.from_dict(repeted_books, orient='index', columns = ["copies"]).rename_axis('title').reset_index()
+  return copies_dataframe

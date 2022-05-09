165377af623d5eaa5d718b8f30536f6e2fbffc2f
Felipe Gomes <felipegomes540@gmail.com>
2022-05-09 20:07:13 -0300

    feat: function o clean column

--- dags/cayena_etl/src/domain/transform_data.py
+++ dags/cayena_etl/src/domain/transform_data.py
@@ -9,3 +9,7 @@ def count_books_copies(all_tiles: List[str]) -> dict:
 def create_copies_dataframe(repeted_books: dict) -> pd.DataFrame:
   copies_dataframe = pd.DataFrame.from_dict(repeted_books, orient='index', columns = ["copies"]).rename_axis('title').reset_index()
   return copies_dataframe
+
+def df_map_values(dataframe: pd.DataFrame, new_column: str, target_column: str, dict_map: dict):
+  dataframe[new_column] = dataframe[target_column].map(dict_map)
+  return dataframe
